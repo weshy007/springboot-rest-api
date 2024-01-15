14818936e513379edce8f570866db42957164851
Weshy Joe <josephwaweru96@gmail.com>
2024-01-15 13:35:19 +0300

    auth controller

---
+++ src/main/java/com/weshy/springrestapi/controllers/AuthController.java
@@ -0,0 +1,54 @@
+package com.weshy.springrestapi.controllers;
+
+import com.weshy.springrestapi.auth.JwtUtil;
+import com.weshy.springrestapi.models.User;
+import com.weshy.springrestapi.models.requests.LoginRequest;
+import com.weshy.springrestapi.models.response.ErrorResponse;
+import com.weshy.springrestapi.models.response.LoginResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/rest/auth")
+public class AuthController {
+    private final AuthenticationManager authenticationManager;
+
+    private JwtUtil jwtUtil;
+    public AuthController(AuthenticationManager authenticationManager, JwtUtil jwtUtil) {
+        this.authenticationManager = authenticationManager;
+        this.jwtUtil = jwtUtil;
+    }
+    @ResponseBody
+    @RequestMapping(value = "/login",method = RequestMethod.POST)
+    public ResponseEntity login(@RequestBody LoginRequest loginRequest)  {
+        try {
+            Authentication authentication =
+                    authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginReq.getEmail(), loginReq.getPassword()));
+
+            String email = authentication.getName();
+            User user = new User(email,"");
+            String token = jwtUtil.createToken(user);
+            LoginResponse loginRes = new LoginResponse(email,token);
+
+            return ResponseEntity.ok(loginRes);
+
+        }catch (BadCredentialsException e){
+
+            ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST,"Invalid username or password");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+        }catch (Exception e){
+
+            ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST, e.getMessage());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+        }
+    }
+}
