6132f7722a19d92fdb48f1b1ddf2efb8c3d0b42f
Weshy Joe <josephwaweru96@gmail.com>
2024-01-15 12:49:12 +0300

    model responses

---
+++ src/main/java/com/weshy/springrestapi/models/response/LoginResponse.java
@@ -0,0 +1,13 @@
+package com.weshy.springrestapi.models.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginResponse {
+    private String email;
+    private String token;
+}
