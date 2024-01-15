51e5ee506232a06f305b2bfb5f936d5fe2fcb010
Weshy Joe <josephwaweru96@gmail.com>
2024-01-15 16:57:24 +0300

    user repo interface

---
+++ src/main/java/com/weshy/springrestapi/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.weshy.springrestapi.repository;
+
+import com.weshy.springrestapi.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    @Query("SELECT u FROM User u WHERE u.email = :email")
+    User findUsersByEmail(@Param("email") String email);
+
+}
