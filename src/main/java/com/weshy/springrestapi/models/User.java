9a54d6b5f447f66a59e394d422a6f6c3c9b1703d
Weshy Joe <josephwaweru96@gmail.com>
2024-01-15 16:54:21 +0300

    updated user model with db

--- src/main/java/com/weshy/springrestapi/models/User.java
+++ src/main/java/com/weshy/springrestapi/models/User.java
@@ -1,20 +1,54 @@
 package com.weshy.springrestapi.models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Date;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "USERS")
 public class User {
+    @Id
+    @Column(name = "ID")
+    private long id;
+    @Column(name = "EMAIL")
     private String email;
+    @Column(name = "PASSWORD")
     private String password;
+    @Column(name = "FIRST_NAME")
     private String firstName;
+    @Column(name = "LAST_NAME")
     private String lastName;
+    @Column(name = "PROFILE_PHOTO_URL")
+    private String profilePhotoUrl;
+    @Column(name = "COUNTRY")
+    private String country;
+    @Column(name = "OCCUPATION")
+    private String occupation;
+    @Column(name = "DATE_OF_BIRTH")
+    private Date dateOfBirth;
+    @Column(name = "GENDER")
+    private String gender;
+    @Column(name = "ROLE")
+    private String role;
 
     public User(String email, String password) {
         this.email = email;
         this.password = password;
+    }
+
+    public User(String email, String password, String firstName, String lastName) {
+        this.email = email;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
     }
 }
